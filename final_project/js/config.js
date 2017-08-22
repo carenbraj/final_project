d2c97ac076a4a06880f277198c1a2a0abf40ecb7
Caren Braj <carenbraj@users.noreply.github.com>
2017-08-21 18:46:07 -0700

    Add files via upload

---
+++ final_project/js/config.js
@@ -0,0 +1,5 @@
+var config = {
+"BBCApi": "https://newsapi.org/v1/articles?source=bbc-news&sortBy=top&apiKey=f10a1ffd68394688bc7671f03cd74792",
+"NYTApi": "https://api.nytimes.com/svc/topstories/v2/home.json?api-key=b5f2056ee8f04f29b0e2993ca360a7c8",
+"WPApi": "https://newsapi.org/v1/articles?source=the-washington-post&sortBy=top&apiKey=f10a1ffd68394688bc7671f03cd74792"
+}
